279fd46822f07dd99313c4dde19c90bc7dc16645
jalehoux <jalehoux@gmail.com>
2018-11-23 07:53:08 -0500

    try this

--- code/routes/index.js
+++ code/routes/index.js
@@ -7,7 +7,7 @@ router.use("/api", apiRoutes);
 
 //If no routes are hit send files
 router.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
