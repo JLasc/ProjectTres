08b4ebd478152d1d26a418ddc6fe14cd43067251
jalehoux <jalehoux@gmail.com>
2018-11-26 19:41:16 -0500

    try again

--- code/routes/index.js
+++ code/routes/index.js
@@ -6,8 +6,8 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 //If no routes are hit send files
-// router.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
