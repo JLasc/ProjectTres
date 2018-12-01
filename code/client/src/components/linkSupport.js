6ab44a97a81b0453065678933196defc7d58501d
aroarian <aroarian@hotmail.com>
2018-12-01 11:53:00 -0500

    qwergthyj

--- code/client/src/components/linkSupport.js
+++ code/client/src/components/linkSupport.js
@@ -1,13 +1,13 @@
 import React from "react";
 import support from "../support.svg";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Support = props => {
   return (
          
       <span>
         <img src={support} alt="" />
-        <Link to="/support" className={props.cart ? "active" : " "}>Support</Link>
+        <NavLink activeClassName="activeLink" to="/support">Support</NavLink>
         </span>
       
   
