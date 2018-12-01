1c6d1c38530092218ef6db271831b0b95f3731be
aroarian <aroarian@hotmail.com>
2018-12-01 13:03:59 -0500

    asdfghj

--- code/client/src/views/OrderHistory.js
+++ code/client/src/views/OrderHistory.js
@@ -2,11 +2,29 @@ import React from "react";
 import Construction from "../../src/construction.png"
 
 
-const orderHistory = () => {
+const orderHistory = (props) => {
+  const displayHistory = props.history.map((each, i) => (
+    <div className="eachComplete" key={i}>
+    <p>{each.x}</p> <p>{each.number}</p><p>{each.date}</p><p>${each.total}</p>
+  </div>
+  ))
+
   return (
-    <div className="construction">
-      <img src={Construction}/>
+    <div>
+    <div className="completelabels">
+      <div>Order Number</div>
+      <div>Item Amount</div>
+      <div>
+        <div className="qtyPrice">Date</div>
       </div>
+      <div>Order Total</div>
+      <div />
+    </div>
+    <div className="completeContainer">
+    {displayHistory}
+    </div>
+    
+  </div>
   
   );
 };
